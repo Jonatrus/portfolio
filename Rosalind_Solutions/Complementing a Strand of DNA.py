b6774bea54b13ef1837deb2d8cac431c62bc0616
Jonny Kobrus <18jkobrus@gmail.com>
2026-06-26 23:22:26 +0100

    reorganised repository. each project became its own repository. updated index.html

--- Rosalind_Solutions/Complementing a Strand of DNA.py
+++
@@ -1,9 +0,0 @@
-with open("rosalind_revc.txt") as f:
-    seq = f.read().strip()
-
-myTable = str.maketrans("ACGT", "TGCA")
-seq = seq.translate(myTable)
-seq = seq[::-1]
-
-with open("output.txt", "w") as f:
-    f.write(seq)
--- Routine_Analysis_of_Bacterial_WGS_Data/main.py
+++
@@ -1,7 +0,0 @@
-# COmmands run for pipeline
-
-"""
-in the data/ folder
-
-fastp -i raw/SRR21931766_1.fastq.gz -I raw/SRR21931766_2.fastq.gz -o preprocessed/SRR21931766_1.fastq.gz -O preprocessed/SRR21931766_2.fastq.gz
-"""
